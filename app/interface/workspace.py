edc8dd0bfbbd67103989e07e5a3940905087a8c3
Piotrek <pibaj@ciop.pl>
2021-03-18 12:15:14 +0100

    ⏩ Optimized panning view with padding pixels

--- app/interface/workspace.py
+++ app/interface/workspace.py
@@ -14,11 +14,13 @@ class WorkspaceView(View):
         self.canvas:tk.Canvas = None
         self._scale = 1.0
         self._text_id = None
-        self._img_scaled = None
+        self._img = None # Original image
+        self._img_scaled = None # Scaled image
+        self._img_cropped = None # Cropped image
         self._img_id = None
-        self._img = None
         self._tkimg = None
         self._line_ids = []
+        self._motion_pos = None
 
     def init(self):
         '''
@@ -30,6 +32,7 @@ class WorkspaceView(View):
         self.canvas.bind('<Button-5>',   self._wheel)  # only with Linux, wheel scroll down
         self.canvas.bind('<Button-4>',   self._wheel)
         self.canvas.bind('<ButtonPress-1>', lambda event: self.canvas.scan_mark(event.x, event.y))
+        self.canvas.bind('<ButtonRelease-1>', self._motion_end)
         self.canvas.bind("<B1-Motion>", self._motion)
 
         self._text_id = self.canvas.create_text(0, 0, anchor='nw', text='Scroll to zoom')
@@ -51,8 +54,13 @@ class WorkspaceView(View):
         # Rescale all canvas objects
         mouse_x, mouse_y = self.canvas.canvasx(event.x), self.canvas.canvasy(event.y)
         self.canvas.scale('all', mouse_x, mouse_y, scale, scale)
+        for line_id in self._line_ids:
+            self.canvas.itemconfig(line_id, width=int(self._scale))
         # Scale image
-        if self._tkimg is not None: self._update_image()
+        if self._tkimg is not None:
+            self._scale_image()
+            self._crop_image()
+            self._update_image()
         # Constrain view box
        # self.canvas.configure(scrollregion=self.canvas.bbox('all'))
         self.canvas.itemconfig(self._text_id, text=f'x{self._scale}')
@@ -60,21 +68,26 @@ class WorkspaceView(View):
     def _motion(self, event):
         # Drag canvas
         self.canvas.scan_dragto(event.x, event.y, gain=1)
+        # Update image every few pixels
+        pos = (event.x, event.y)
+        if self._motion_pos is None: self._motion_pos = pos
+        dist = abs(pos[0] - self._motion_pos[0])**2 + abs(pos[1] - self._motion_pos[1])**2
+        if dist > 50**2:
+            self._motion_pos = pos
+            self._motion_end(event)
+
+    def _motion_end(self, event):
         # Update image
+        self._crop_image()
         self._update_image()
 
-    def _update_image(self):
-        # Remove previous
-        if self._img_id:
-            self.canvas.delete(self._img_id)
-            self._tkimg = None
-            self._img_scaled = None
-            gc.collect()
-
+    def _scale_image(self):
         # Calculate new size
-        img_size = self._img.size
-        new_size = int(self._scale * img_size[0]), int(self._scale * img_size[1])
+        new_size = int(self._scale * self._img.size[0]), int(self._scale * self._img.size[1])
+        # Scale image
+        self._img_scaled = self._img.resize(new_size, Image.NEAREST)
 
+    def _crop_image(self):
         # View box in canvas space
         pos = self.canvas.coords(self._text_id)
         pos = [int(pos[0]), int(pos[1])]
@@ -86,24 +99,35 @@ class WorkspaceView(View):
         )
 
         # Image box visible in view box
+        new_size = self._img_scaled.size
+        padding = 50
         crop = (
-            min(new_size[0], max(view_box[0], 0)), # Left
-            min(new_size[1], max(view_box[1], 0)), # Upper
-            new_size[0] - min(new_size[0], max(new_size[0]-view_box[2], 0)), # Right
-            new_size[1] - min(new_size[1], max(new_size[1]-view_box[3], 0)), # Bottom
+            min(new_size[0], max(view_box[0] - padding, 0)), # Left
+            min(new_size[1], max(view_box[1] - padding, 0)), # Upper
+            new_size[0] - min(new_size[0], max(new_size[0]-view_box[2]-padding, 0)), # Right
+            new_size[1] - min(new_size[1], max(new_size[1]-view_box[3]-padding, 0)), # Bottom
         )
 
-        # Adjust image position
-        pos[0] += crop[0]
-        pos[1] += crop[1]
+        # Save image position
+        self._img_pos = (
+            crop[0] + pos[0],
+            crop[1] + pos[1],
+        )
 
-        # Create scaled image
-        self._img_scaled = self._img.resize(new_size, Image.NEAREST)
-        self._img_scaled = self._img_scaled.crop(box=crop)
-        self._tkimg = ImageTk.PhotoImage(image=self._img_scaled)
-        # Add to canvas
+        # Crop image
+        self._img_cropped = self._img_scaled.crop(box=crop)
+
+    def _update_image(self):
+        # Remove previous
+        if self._img_id:
+            self.canvas.delete(self._img_id)
+            self._tkimg = None
+            gc.collect()
+
+        # Add new
+        self._tkimg = ImageTk.PhotoImage(image=self._img_cropped)
         self.canvas.delete('img')
-        self._img_id = self.canvas.create_image(pos, anchor='nw', image=self._tkimg, tag='img')
+        self._img_id = self.canvas.create_image(self._img_pos, anchor='nw', image=self._tkimg, tag='img')
         self.canvas.lower(self._img_id)
 
     def show(self, image:RasterImage):
@@ -117,6 +141,8 @@ class WorkspaceView(View):
 
         # Display raster image
         self._img = image.image
+        self._scale_image()
+        self._crop_image()
         self._update_image()
 
         # Display polygons
